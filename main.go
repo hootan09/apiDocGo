578b464831fb3d292f2c27281c892b55b56905e7
mam niki <hootan09@gmail.com>
2022-05-28 17:41:59 +0430

    small change

--- main.go
+++ main.go
@@ -6,8 +6,17 @@ import (
 )
 
 func main() {
-	Documents := parser.ReadDoc("./examples")
-	fmt.Printf("%+v \n", Documents)
+	// Documents := parser.ReadDoc("./examples")
+	// fmt.Printf("%+v \n", Documents)
+
+	result := parser.Api("{POST} /user/:region/:id/:opt Read data of a User", true)
+	fmt.Printf("%+v \n", result)
+
+	// json_data, err := json.Marshal(Documents)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// fmt.Println(string(json_data))
 
 	//TODO
 	// usage "apidocgo -i routes/ -o public/apidoc"
