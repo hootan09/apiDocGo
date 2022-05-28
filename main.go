49b9ab6fd541be61b42d7545cf80453c36b7a711
mam niki <hootan09@gmail.com>
2022-05-28 18:34:22 +0430

    main debug

--- main.go
+++ main.go
@@ -2,19 +2,18 @@ package main
 
 import (
 	"ApiDocGo/parser"
-	"encoding/json"
 	"fmt"
 )
 
 func main() {
 	Documents := parser.ReadDoc("./examples")
-	// fmt.Printf("%+v \n", Documents)
+	fmt.Printf("%+v \n", Documents)
 
-	json_data, err := json.Marshal(Documents)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(string(json_data))
+	// json_data, err := json.Marshal(Documents)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// fmt.Println(string(json_data))
 
 	//TODO
 	// usage "apidocgo -i routes/ -o public/apidoc"
